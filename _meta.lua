687f8a75e6dd0de99471de259529cb6f8d4ea5d4
Euphoriyy <jstkidding@mail.com>
2026-04-30 02:48:05 -0400

    chore: add name field and tweak description of plugin

--- _meta.lua
+++ _meta.lua
@@ -1,6 +1,7 @@
 local _ = require("gettext")
 return {
+    name = "appearance",
     fullname = _("Appearance"),
     description = _(
-        [[Customize KOReader's look with themes, background/font colors, page colors, and background images.]]),
+        [[Customize KOReader's look with themes, background/font colors, page colors, background images, etc.]]),
 }
