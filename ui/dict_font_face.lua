e9d6e9766836fa25829de275db02f8f6187db5db
Euphoriyy <jstkidding@mail.com>
2026-05-03 15:26:06 -0400

    feat(ui): add optional system fonts support
    Fixes crashes when system fonts are enabled.

--- ui/dict_font_face.lua
+++ ui/dict_font_face.lua
@@ -2,6 +2,7 @@
 -- https://github.com/gennaro-tedesco/KOReader.patches/blob/master/patches/2-custom-ui-fonts.lua
 -- by @gennaro-tedesco
 
+local Device = require("device")
 local DictQuickLookup = require("ui/widget/dictquicklookup")
 local Font = require("ui/font")
 local FontList = require("fontlist")
@@ -11,6 +12,7 @@ local cre = require("document/credocument"):engineInit()
 
 local DictFontName = Setting("dict_font_name", "Noto Sans")
 local DictFontEnabled = Setting("dict_font_enabled", true)
+local SystemFonts = Setting("system_fonts", false)
 
 local font_list
 local fonts
@@ -44,19 +46,21 @@ local function init()
     -- Get fonts from CRE font list
     for _, name in ipairs(cre.getFontFaces()) do
         local path_regular = cre.getFontFaceFilenameAndFaceIndex(name)
-        local path_bold = get_bold_path(path_regular)
+        if path_regular then
+            local path_bold = get_bold_path(path_regular)
 
-        local regular_exists = path_exists[path_regular]
-        local bold_exists = path_exists[path_bold]
+            local regular_exists = path_exists[path_regular]
+            local bold_exists = path_exists[path_bold]
 
-        table.insert(font_list, name)
-        -- Add both regular and bold versions if they exist, otherwise default to whatever is left
-        if regular_exists and bold_exists then
-            fonts[name] = { regular = path_regular, bold = path_bold }
-        elseif regular_exists then
-            fonts[name] = { regular = path_regular, bold = path_regular }
-        elseif bold_exists then
-            fonts[name] = { regular = path_bold, bold = path_bold }
+            table.insert(font_list, name)
+            -- Add both regular and bold versions if they exist, otherwise default to whatever is left
+            if regular_exists and bold_exists then
+                fonts[name] = { regular = path_regular, bold = path_bold }
+            elseif regular_exists then
+                fonts[name] = { regular = path_regular, bold = path_regular }
+            elseif bold_exists then
+                fonts[name] = { regular = path_bold, bold = path_bold }
+            end
         end
     end
 end
@@ -73,15 +77,29 @@ local function dict_font_face_menu()
             return T(_("Dictionary font: %1"), DictFontEnabled.get() and DictFontName.get() or "default")
         end,
         sub_item_table_func = function()
-            local items = {}
-            table.insert(items, {
-                text = "Enable font replacement",
-                checked_func = DictFontEnabled.get,
-                callback = function()
-                    DictFontEnabled.toggle()
-                end,
-                separator = true,
-            })
+            local items = {
+                {
+                    text = "Enable font replacement",
+                    checked_func = DictFontEnabled.get,
+                    callback = function()
+                        DictFontEnabled.toggle()
+                    end,
+                    separator = true,
+                },
+            }
+            -- Add option for toggling system fonts on supported platforms
+            if Device:isAndroid() or Device:isDesktop() or Device:isEmulator() or Device:isPocketBook() then
+                items[1].separator = nil
+                table.insert(items, {
+                    text = "Enable system fonts",
+                    checked_func = SystemFonts.get,
+                    callback = function()
+                        SystemFonts.toggle()
+                        UIManager:askForRestart()
+                    end,
+                    separator = true,
+                })
+            end
             for i, name in ipairs(font_list) do
                 table.insert(items, {
                     text = T(name .. " %1", fonts[name].regular == fonts[name].bold and "(no bold)" or ""),
--- ui/font_face.lua
+++ ui/font_face.lua
@@ -1,6 +1,7 @@
 -- Based on patch:
 -- https://github.com/sebdelsol/KOReader.patches/blob/main/2--ui-font.lua by @sebdelsol
 
+local Device = require("device")
 local Font = require("ui/font")
 local FontList = require("fontlist")
 local Setting = require("lib/setting")
@@ -9,6 +10,7 @@ local cre = require("document/credocument"):engineInit()
 
 local UIFontName = Setting("ui_font_name", "Noto Sans")
 local UIFontEnabled = Setting("ui_font_enabled", true)
+local SystemFonts = Setting("system_fonts", false)
 
 local font_list
 local fonts
@@ -54,19 +56,21 @@ local function init(font_name)
     -- Get fonts from CRE font list
     for _, name in ipairs(cre.getFontFaces()) do
         local path_regular = cre.getFontFaceFilenameAndFaceIndex(name)
-        local path_bold = get_bold_path(path_regular)
+        if path_regular then
+            local path_bold = get_bold_path(path_regular)
 
-        local regular_exists = path_exists[path_regular]
-        local bold_exists = path_exists[path_bold]
+            local regular_exists = path_exists[path_regular]
+            local bold_exists = path_exists[path_bold]
 
-        table.insert(font_list, name)
-        -- Add both regular and bold versions if they exist, otherwise default to whatever is left
-        if regular_exists and bold_exists then
-            fonts[name] = { regular = path_regular, bold = path_bold }
-        elseif regular_exists then
-            fonts[name] = { regular = path_regular, bold = path_regular }
-        elseif bold_exists then
-            fonts[name] = { regular = path_bold, bold = path_bold }
+            table.insert(font_list, name)
+            -- Add both regular and bold versions if they exist, otherwise default to whatever is left
+            if regular_exists and bold_exists then
+                fonts[name] = { regular = path_regular, bold = path_bold }
+            elseif regular_exists then
+                fonts[name] = { regular = path_regular, bold = path_regular }
+            elseif bold_exists then
+                fonts[name] = { regular = path_bold, bold = path_bold }
+            end
         end
     end
 
@@ -93,17 +97,31 @@ local function font_face_menu()
             return T(_("Font: %1"), UIFontEnabled.get() and UIFontName.get() or "default")
         end,
         sub_item_table_func = function()
-            local items = {}
-            table.insert(items, {
-                text = "Enable font replacement",
-                checked_func = UIFontEnabled.get,
-                callback = function()
-                    UIFontEnabled.toggle()
-                    apply_font()
-                    UIManager:askForRestart(_("Restart to fully apply the UI font change."))
-                end,
-                separator = true,
-            })
+            local items = {
+                {
+                    text = "Enable font replacement",
+                    checked_func = UIFontEnabled.get,
+                    callback = function()
+                        UIFontEnabled.toggle()
+                        apply_font()
+                        UIManager:askForRestart(_("Restart to fully apply the UI font change."))
+                    end,
+                    separator = true,
+                },
+            }
+            -- Add option for toggling system fonts on supported platforms
+            if Device:isAndroid() or Device:isDesktop() or Device:isEmulator() or Device:isPocketBook() then
+                items[1].separator = nil
+                table.insert(items, {
+                    text = "Enable system fonts",
+                    checked_func = SystemFonts.get,
+                    callback = function()
+                        SystemFonts.toggle()
+                        UIManager:askForRestart()
+                    end,
+                    separator = true,
+                })
+            end
             for i, name in ipairs(font_list) do
                 table.insert(items, {
                     text = T(name .. " %1", fonts[name].regular == fonts[name].bold and "(no bold)" or ""),
