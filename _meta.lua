83850c14eeb5cb56283483e2b5cf7f66ca24a84c
Euphoriyy <jstkidding@mail.com>
2026-06-07 18:24:51 -0400

    fix(meta): remove deprecated name field
    Related to: #67

--- _meta.lua
+++ _meta.lua
@@ -1,6 +1,5 @@
 local _ = require("gettext")
 return {
-    name = "appearance",
     fullname = _("Appearance"),
     description = _(
         [[Customize KOReader's look with themes, background/font colors, page colors, background images, etc.]]),
