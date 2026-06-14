ebd1d1d898584ec117b244dbc7305e66f4aefd45
Euphoriyy <jstkidding@mail.com>
2026-06-14 15:16:26 -0400

    chore: bump plugin version to 1.5.1
    Related to: #60

--- _meta.lua
+++ _meta.lua
@@ -3,5 +3,5 @@ return {
     fullname = _("Appearance"),
     description = _(
         [[Customize KOReader's look with themes, background/font colors, page colors, background images, etc.]]),
-    version = "1.5.0",
+    version = "1.5.1",
 }
